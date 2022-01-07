f0ba8beda5500f0bac22a9acbf5083f0975e033d
Meshuka <meshukarayamajhi@gmail.com>
2022-01-07 13:52:12 +0545

    frontend ui initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Dashboard from './Views/Pages/Dashboard';
+import Sidenavbar from './Components/Sidenavbar';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Signin from './Views/Account Pages/Signin';
+import Register from './Views/Account Pages/Register';
+import Profile from './Views/Account Pages/Profile';
+import Tables from './Views/Pages/Tables';
+import Editprofile from './Views/Account Pages/Editprofile';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Dashboard/>}></Route>
+        <Route exact path="/dashboard" element={<Dashboard />}></Route>
+        <Route exact path="/signin" element={<Signin />}></Route>
+        <Route exact path="/register" element={<Register />}></Route>
+        <Route exact path="/profile" element={<Profile />}></Route>
+        <Route exact path="/tables" element={<Tables />}></Route>
+        <Route exact path="/editprofile" element={<Editprofile />}></Route>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
