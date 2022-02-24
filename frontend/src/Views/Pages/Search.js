192cedca1834fe3c9fb2e3d4a52193f4f225e111
reetipd <reetipd@gmail.com>
2022-02-24 16:08:09 +0545

    register user validation

--- frontend/src/Views/Pages/Search.js
+++ frontend/src/Views/Pages/Search.js
@@ -114,7 +114,9 @@ function Search() {
                       }}
                     />
                   </div>
-                  {hasError && <p>All fields are required</p>}
+                  {hasError && (
+                    <p style={{ color: "red" }}>All fields are required</p>
+                  )}
                 </div>
 
                 <div class="mt-5 text-center">
